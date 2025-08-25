bef5bbd9924ae63beb5a53f5eeb98ba2dc7e2a46
FelipeMnedesSilva <febasmendes@gmail.com>
2025-08-25 14:47:39 -0300

    dto

---
+++ BNP.CMM.Application/DTO/FunctionManualMovementsResult.cs
@@ -0,0 +1,13 @@
+﻿namespace BNP.CMM.Application.DTO
+{
+    public class FunctionManualMovementsResult
+    {
+        public int Month { get; set; }
+        public int Year { get; set; }
+        public required string ProductId { get; set; }
+        public required string ProductDescription { get; set; }
+        public long EntryNumber { get; set; }
+        public required string Description { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
